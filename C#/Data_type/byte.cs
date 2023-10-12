7b83c493d859835c29fee2fa0caa49f15bbbf393
im_sujit.sk <138555674+sujitsk2912@users.noreply.github.com>
2023-10-12 14:41:11 +0530

    c sharp

---
+++ C#/Data_type/byte.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Byte
+{
+    class Byte
+    {
+        public static void Main()
+        {
+            byte b; // Range of byte is 0 to 255 ( negative value does not exist) 
+            Console.Write("Enter Byte: ");
+            b = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Byte value: " + b);
+            Console.ReadKey();
+        }
+    }
+}
