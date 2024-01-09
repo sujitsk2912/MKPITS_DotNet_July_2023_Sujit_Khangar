8b9ddc4c4380a18347a0ae2cd24f4365dc179dff
im_sujit.sk <138555674+sujitsk2912@users.noreply.github.com>
2024-01-09 20:35:20 +0530

    9 jan

--- ADO.NET/Fendahl_Machine_Test/Fendal_Machine_Test1.sql
+++ ADO.NET/Fendahl_Machine_Test/Fendal_Machine_Test1.sql
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Fendal_Machine_Test1
 {
@@ -25,13 +24,8 @@ namespace Fendal_Machine_Test1
             InitializeComponent();
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        enum Category { Student, IT_Proffesional }
-        enum Gender { Male, Female, Others }
+        public enum Category { Student, IT_Proffesional }
+        public enum Gender { Male, Female, Others }
 
         Category c;
         Gender g;
@@ -58,6 +52,9 @@ namespace Fendal_Machine_Test1
             dateTimePicker.Text = DateTime.Now.ToString();
         }
 
+        float TotalFeesPaid = 0;
+        float BalanceAmt = 0;
+
         public void Calculate()
         {
             if (btnStudent.Checked)
@@ -66,16 +63,17 @@ namespace Fendal_Machine_Test1
 
                 float TotalAmt = Convert.ToSingle(txtTotalAmt.Text);
 
-                float TotalFeesPaid = TotalAmt * 0.5f;
+                 TotalFeesPaid = TotalAmt * 0.5f;
 
                 if(Convert.ToSingle(txtFeesPaid.Text) < TotalFeesPaid)
                 {
                     MessageBox.Show("Minimum amount should be : " + TotalFeesPaid);
+                    txtBalanceAmt.Text = "0";
                 }    
                 else
                 {
-                    float Total = TotalAmt - Convert.ToSingle(txtFeesPaid.Text);
-                    txtBalanceAmt.Text = Total.ToString();
+                     BalanceAmt = TotalAmt - Convert.ToSingle(txtFeesPaid.Text);
+                     txtBalanceAmt.Text = BalanceAmt.ToString();
                 }
             }
 
@@ -85,16 +83,17 @@ namespace Fendal_Machine_Test1
 
                 float TotalAmt = Convert.ToSingle(txtTotalAmt.Text);
 
-                float TotalFeesPaid = TotalAmt * 0.8f;
+                 TotalFeesPaid = TotalAmt * 0.8f;
 
                 if (Convert.ToSingle(txtFeesPaid.Text) < TotalFeesPaid)
                 {
                     MessageBox.Show("Minimum amount should be : " + TotalFeesPaid);
+                    txtBalanceAmt.Text = "0";
                 }
                 else
                 {
-                    float Total = TotalAmt - Convert.ToSingle(txtFeesPaid.Text);
-                    txtBalanceAmt.Text = Total.ToString();
+                     BalanceAmt = TotalAmt - Convert.ToSingle(txtFeesPaid.Text);
+                     txtBalanceAmt.Text = BalanceAmt.ToString();
                 }
             }
         }
@@ -178,16 +177,6 @@ namespace Fendal_Machine_Test1
            GetCity();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBoxCity_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnStudent_CheckedChanged(object sender, EventArgs e)
         {
             ButtonStudent();
@@ -222,17 +211,119 @@ namespace Fendal_Machine_Test1
             Calculate();
         }
 
-        private void txtFeesPaid_TextChanged(object sender, EventArgs e)
+        public void InsertCourseRegDetails(Category cat, Gender gen)
+        {
+           
+            conn = new SqlConnection(connection);
+
+            try 
+            {
+                conn.Open();
+                string Query = "Insert into TableCourseRegDetail values (@CategoryInd, @FullName, @GenderInd)";
+                SqlCommand cmd = new SqlCommand(Query,conn);
+                cmd.Parameters.AddWithValue("@CategoryInd", Convert.ToInt32(cat));
+                cmd.Parameters.AddWithValue("@FullName", txtFullName.Text);
+                cmd.Parameters.AddWithValue("@GenderInd", Convert.ToInt32(gen));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Course Details Inserted");
+                InsertRegAddress();
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close(); 
+            }
+
+
+        }
+        public void InsertRegAddress()
+        {
+            conn = new SqlConnection (connection);
+
+            try
+            {
+                conn.Open();
+                string Query1 = "Select Max(CourseregID) from TableCourseRegDetail";
+                SqlCommand cmd1 = new SqlCommand(Query1,conn);
+
+                int MaxCourseRegID = Convert.ToInt32(cmd1.ExecuteScalar());
+                
+                string Query2 = "Insert into TableRegAddress values (@CourseregID, @NationID, @StateID, @CityID) ";
+                SqlCommand cmd2 = new SqlCommand(Query2,conn);
+                cmd2.Parameters.Add("@CourseregID",SqlDbType.Int).Value = MaxCourseRegID;
+                cmd2.Parameters.Add("@NationID", SqlDbType.Int).Value = comboBoxNation.SelectedValue;
+                cmd2.Parameters.Add("@StateID", SqlDbType.Int).Value = comboBoxState.SelectedValue;
+                cmd2.Parameters.Add("@CityID", SqlDbType.Int).Value = comboBoxCity.SelectedValue;
+                cmd2.ExecuteNonQuery();
+                MessageBox.Show("Register Details Inserted");
+                InsertFeeDetails();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        public void InsertFeeDetails()
         {
 
+            conn = new SqlConnection(connection);
+
+            try
+            {
+                conn.Open();
+                string Query1 = "Select Max(CourseRegID) from TableCourseRegDetail";
+                SqlCommand cmd1 = new SqlCommand(Query1,conn);
+
+                int MaxCourseRegID = Convert.ToInt32(cmd1.ExecuteScalar());
+
+                string Query2 = "Insert into TableFeeDetail values (@CourseRegID, @TotalAmount, @MinPer, @PaidAmount, @BalAmount, @PaidDate)";
+                SqlCommand cmd2 = new SqlCommand(Query2,conn);
+                cmd2.Parameters.Add("@CourseRegID",SqlDbType.Int).Value = MaxCourseRegID;
+                cmd2.Parameters.Add("@TotalAmount", SqlDbType.Decimal).Value = txtTotalAmt.Text;
+                cmd2.Parameters.Add("@MinPer", SqlDbType.Decimal).Value = TotalFeesPaid;
+                cmd2.Parameters.Add("@PaidAmount", SqlDbType.Decimal).Value = txtFeesPaid.Text;
+                cmd2.Parameters.Add("@BalAmount", SqlDbType.Decimal).Value = txtBalanceAmt.Text;
+                cmd2.Parameters.Add("@PaidDate", SqlDbType.DateTime).Value = dateTimePicker.Value;
+                cmd2.ExecuteNonQuery();
+                MessageBox.Show("Fees Details Inserted");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if(txtFullName.Text == "")
+            // DateTime
+
+            DateTime selectDate = dateTimePicker.Value;
+
+            if( selectDate < DateTime.Today)
             {
-                MessageBox.Show("Fullname cannot be blank");
+                MessageBox.Show("Please select a payment date that is on or after today's date.","DateTime Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
+            //FullName not Blank
+
+            else if(txtFullName.Text == "")
+            {
+                MessageBox.Show("Fullname can'not be blank");
+            }
+
             else
             {
                 // Category
@@ -262,6 +353,10 @@ namespace Fendal_Machine_Test1
                     g = Gender.Others;
                 }
                 MessageBox.Show("Gender: " +  g.ToString());
+
+                InsertCourseRegDetails(c, g);
+
+                MessageBox.Show("Data Inserted Successfully..");
             }
         }
 
@@ -270,6 +365,7 @@ namespace Fendal_Machine_Test1
             btnStudent.Select();
             btnMale.Select();
             txtFullName.Clear();
+
             if(btnStudent.Checked)
             {
                 ButtonStudent();
@@ -278,6 +374,7 @@ namespace Fendal_Machine_Test1
             {
                 ButtonITProffesional();
             }
+
             dateTimePicker.Value = DateTime.Now;
         }
     }
